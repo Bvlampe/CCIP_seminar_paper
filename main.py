7a0f142f6f0282dd7802e2b35df4b1024a2960d8
Bjarne <bjarne@vonlampe.de>
2022-10-22 16:07:26 +0200

    Big changes but not quite there yet

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 import pandas as pd
 
-
-document = True
+document = False
 
 loc_homicides = "homicides.csv"
 loc_ged = "GED_cleaned.csv"
-loc_conflict_end ="ucdp-term-acd-3-2021.csv"
+loc_conflict_end = "ucdp-term-acd-3-2021.csv"
 loc_conflict_all = "ucdp-prio-acd-221.csv"
-
-
-def ccy_create(row):
-    return tuple([row["country"], row["conflict_new_id"], row["year"]])
+loc_merged_conflicts = "merged_conflicts.xlsx"
 
 
 def main():
     # Read datasets
-    homicides = pd.read_csv(loc_homicides, sep=';')
+    homicides = pd.read_csv(loc_homicides)
     ged = pd.read_csv(loc_ged)
-    conflict_end = pd.read_csv(loc_conflict_end, sep=';')
+    conflict_end = pd.read_csv(loc_conflict_end)
     if document: print("Shape of GED before cleaning up inactive year events:", ged.shape)
     ged_active = ged[ged["active_year"] == 1]
     if document: print("Shape of GED after cleaning up inactive year events:", ged_active.shape)
-    conflict_all = pd.read_csv(loc_conflict_all, sep=';')
+    conflict_all = pd.read_csv(loc_conflict_all)
 
-    return 0
-    # conf_end = conflict[conflict["ep_end_date"] == 1]
-    # if document: print("Shape of ACD after cleaning up ongoing conflicts:", conf_end.shape)
+    # Filter the ACD to exclude rows of non-ending conflicts to avoid doubles
+    conflict_new = conflict_all[["conflict_id", "start_date2", "ep_end", "ep_end_date", "year"]]
 
-    conflict_new = conflict_all[["conflict_id", "start_date2", "ep_end", "ep_end_date"]]
-    conflict_new = conflict_new[conflict_new["ep_end"] == 1]
+    # Mark the rows of conflicts that are over
+    df = conflict_new.loc[:, ["conflict_id", "start_date2", "ep_end"]].groupby(["conflict_id", "start_date2"]).sum()
+    df.reset_index(inplace=True)
+    df.rename(columns={"ep_end":"has_ended"}, inplace=True)
+    conflict_new = conflict_new.merge(df, left_on=["conflict_id", "start_date2"], right_on=["conflict_id", "start_date2"])
 
-    # Prep the GED dataset for merging all events with the same CCY into one
-    ged_active["CCY"] = ged_active.apply(lambda row: ccy_create(row), axis=1)
-    ccy_merge = pd.DataFrame(ged_active["CCY"].unique())
-    ccy_merge.rename(columns={0 : "CCY"}, inplace=True)
-    ccy_merge["deaths"] = 0
+    # Remove presently ongoing conflicts
+    conflict_new = conflict_new[conflict_new["has_ended"] == 1]
 
-    # Merge GED deaths by CCY
-    for row in ged_active.iterrows():
-        found = ccy_merge.loc[ccy_merge["CCY"] == row["CCY"]]
+    # Fill end date
+    conflict_new["ep_end_date"].fillna(method = "bfill", inplace=True)
+    print(conflict_new.head(10))
 
-    print(ccy_merge.head())
+    # conflict_new = conflict_new[conflict_new["ep_end_date"].notna()]
+    conflict_new["start_year"] = conflict_new["start_date2"].str[:4]
+    conflict_new["end_year"] = conflict_new["ep_end_date"].str[:4]
+    conflict_new["duration"] = conflict_new["end_year"].astype(int) - conflict_new["start_year"].astype(int) + 1
+
+    # Add up deaths per CCY triad
+    ccy_merge = ged_active.loc[:, ["country", "conflict_new_id", "year", "best"]].groupby(
+        by=["country", "conflict_new_id", "year"]).sum()
+
+    ccy_merge.reset_index(inplace=True)
+
+    ccy_merge.rename(columns={"conflict_new_id" : "conflict_id"}, inplace=True)
+    print(ccy_merge.columns)
+    ccy_complete = ccy_merge.merge(conflict_new, left_on=["conflict_id", "year"], right_on=["conflict_id", "year"])
+    print(ccy_complete.head(20))
+    print(ccy_complete.columns)
+
+    ccy_complete.to_excel(loc_merged_conflicts)
 
     # ged_active.to_csv(loc_ged)
     return 0
+
 
 if __name__ == "__main__":
     main()
